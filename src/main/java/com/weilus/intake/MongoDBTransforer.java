a778c9074190086c613bfc93a867da306e7ee0ea
刘太全 <646154945@qq.com>
2019-03-28 16:14:09 +0800

    intake

--- src/main/java/com/weilus/intake/MongoDBTransforer.java
+++ src/main/java/com/weilus/intake/MongoDBTransforer.java
@@ -6,6 +6,9 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,12 +35,22 @@ public class MongoDBTransforer implements LogTransforer{
         List<Document> list = lines.stream()
                 .map(line->
                         new Document()
-                                .append("time",LogParserUtil.getMatcher(REG_DATE_YMDHMSS,line))
+                                .append("time",toDate(LogParserUtil.getMatcher(REG_DATE_YMDHMSS,line)))
                                 .append("level",LogParserUtil.getMatcher(REG_LEVEL,line))
                                 .append("spanid",LogParserUtil.getMatcher(REG_SELTH,line))
                                 .append("logger",LogParserUtil.getMatcher(REG_CLZZ,line))
                                 .append("message",LogParserUtil.parseMsg(line))
         ).collect(Collectors.toList());
         collection.insertMany(list);
+    }
+
+    static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public static Date toDate(String time){
+        try {
+            return SIMPLE_DATE_FORMAT.parse(time);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
